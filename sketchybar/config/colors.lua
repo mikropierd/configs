938acea1340f33d423e3abf571cf23b829d77aee
mikropierd <90505284+mikropierd@users.noreply.github.com>
2025-02-15 08:19:36 +0100

    update

---
+++ sketchybar/config/colors.lua
@@ -0,0 +1,35 @@
+local colors <const> = {
+  black = 0x000000,
+  white = 0xfff8f8f2,
+  red = 0xf1cc3e44,
+  green = 0xff8aff81,
+  blue = 0xff5199ba,
+  yellow = 0xffffff81,
+  orange = 0xfff4c07b,
+  magenta = 0xd3fc7ebd,
+  purple = 0xff796fa9,
+  other_purple = 0xff302c45,
+  cyan = 0xff7bf2de,
+  grey = 0xff7f8490,
+  dirty_white = 0xc8cad3f5,
+  dark_grey = 0xff2b2736,
+  transparent = 0x000000,
+  bar = {
+    bg = 0x000000,
+    border = 0x000000,
+  },
+  popup = {
+    bg = 0x000000,
+    border = 0x000000,
+  },
+  slider = {
+    bg = 0xf1151320,
+    border = 0xff2c2e34,
+  },
+  bg1 = 0xff181819,
+  bg2 = 0xff181819,
+
+ 
+}
+
+return colors
